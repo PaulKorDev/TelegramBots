f2c2773a3e2e8c2c4d13905730b5e97c82499a58
PaulKorDev <147442523+PaulKorDev@users.noreply.github.com>
2023-10-12 03:41:40 +0300

    Delete Анонимные переписки/NoName directory

--- Анонимные переписки/NoName/config.py
+++
@@ -1,6 +0,0 @@
-from aiogram import types
-
-
-#userID = types.Message.from_user.id
-#chatID = types.Message.chat.id
-#messageID = types.Message.message_id
--- Анонимные переписки/NoName/handlers/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-from handlers import module1
-
-"""
